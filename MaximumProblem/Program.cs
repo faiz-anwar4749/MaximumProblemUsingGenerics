a0b9666be4507a047b2b05b788c0b759d1eeaa46
Faiz <faiz.anwar4749@gmail.com>
2020-10-22 15:46:55 +0530

    [Faiz] Refactor: Finding Max of 3 integers using CompareTo method

--- MaximumProblem/Program.cs
+++ MaximumProblem/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace MaximumProblem
 {
     class Program
@@ -7,6 +6,34 @@ namespace MaximumProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum Number Problem");
+            int max = MaxInteger(99, 89, 88);
+            int max1 = MaxInteger(978,999,977);
+            int max2 = MaxInteger(89899, 786547, 789765);
+            Console.WriteLine("The maximum number is: " + max);
+            Console.WriteLine("The maximum number is: " + max1);
+            Console.WriteLine("The maximum number is: " + max2);
+        }
+        public static int MaxInteger(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+              secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+              secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+             thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+             thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
         }
     }
 }
