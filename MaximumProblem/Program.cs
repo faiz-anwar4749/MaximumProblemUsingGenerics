541285f7996435fd1422bec51956b83361c5e86b
Faiz <faiz.anwar4749@gmail.com>
2020-10-22 15:52:42 +0530

    [Faiz] Refactor: Changing Method name

--- MaximumProblem/Program.cs
+++ MaximumProblem/Program.cs
@@ -6,14 +6,14 @@ namespace MaximumProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum Number Problem");
-            int max = MaxInteger(99, 89, 88);
-            int max1 = MaxInteger(978,999,977);
-            int max2 = MaxInteger(89899, 786547, 789765);
+            int max = Max(99, 89, 88);
+            int max1 = Max(978,999,977);
+            int max2 = Max(89899, 786547, 789765);
             Console.WriteLine("The maximum number is: " + max);
             Console.WriteLine("The maximum number is: " + max1);
             Console.WriteLine("The maximum number is: " + max2);
         }
-        public static int MaxInteger(int firstValue, int secondValue, int thirdValue)
+        public static int Max(int firstValue, int secondValue, int thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
